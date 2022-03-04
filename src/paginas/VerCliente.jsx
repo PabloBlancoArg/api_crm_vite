f20ea864851cbd0b3c2eb174b4eabd9161700034
PabloBlancoArg <blancopablo1991@gmail.com>
2022-03-04 16:08:05 -0300

    primer y ultimo commit

---
+++ src/paginas/VerCliente.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Spinner from '../components/Spinner'
+
+const VerCliente = () => {
+
+    const [cliente, setCliente] = useState({})
+    const [cargando, setCargando] = useState(true)
+
+    const { id } = useParams()
+
+    useEffect(() => {
+        setCargando(!cargando)
+        const obtenerClienteAPI = async () => {
+            try {
+                const url = `http://localhost:3001/clientes/${id}`
+                const respuesta = await fetch(url)
+                const resultado = await respuesta.json()
+
+                setCliente(resultado)
+            } catch (error) {
+                console.log(error)
+            }
+            
+            setCargando(!cargando)
+        }
+        obtenerClienteAPI()
+    }, [])
+
+  return (
+
+    cargando ? <Spinner /> : Object.keys(cliente).length === 0 ? <p>No hay resultado</p> : (
+
+    <div>
+        <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
+        <p className='mt-3'>Informacion del cliente</p>
+
+        { cliente.nombre && (
+            <p className='text-2xl text-gray-600 mt-10'>
+                <span className='text-gray-700 uppercase font-bold'>Cliente:</span>
+                {cliente.nombre} 
+            </p>
+        )}
+
+        { cliente.telefono && (
+            <p className='text-2xl text-gray-600 mt-10'>
+                <span className='text-gray-700 uppercase font-bold'>Tel:</span>
+                {cliente.telefono} 
+            </p>
+        )}
+        { cliente.email && (
+            <p className='text-2xl text-gray-700 mt-4'>
+                <span className='text-gray-700 uppercase font-bold'>Email:</span>
+                {cliente.email} 
+            </p>
+        )}
+
+        { cliente.empresa && (
+            <p className='text-2xl text-gray-700 mt-4'>
+                <span className='text-gray-700 uppercase font-bold'>Empresa:</span>
+                {cliente.empresa} 
+            </p>
+        )}
+
+        { cliente.notas && (
+            <p className='text-2xl text-gray-700 mt-4'>
+                <span className='text-gray-700 uppercase font-bold'>Observaciones:</span>
+                {cliente.observaciones} 
+            </p>
+        )}
+        
+    </div>
+    )
+  )
+}
+
+export default VerCliente
